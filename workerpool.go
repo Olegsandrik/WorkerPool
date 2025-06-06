762b13eaeb16025468c2aa86c4aadef24fb83ba1
Alexandr Nikitin <nikitinav2@student.bmstu.ru>
2025-06-06 23:34:59 +0300

    Create workerpool.go

---
+++ workerpool.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	workTime = 2 * time.Second
+)
+
+type WorkerPool struct {
+	input   chan string
+	closers []chan struct{}
+	mutex   sync.Mutex
+	wg      sync.WaitGroup
+}
+
+func NewWorkerPool() *WorkerPool {
+	wp := &WorkerPool{
+		input:   make(chan string),
+		closers: make([]chan struct{}, 0),
+		mutex:   sync.Mutex{},
+		wg:      sync.WaitGroup{},
+	}
+
+	return wp
+}
+
+func (wp *WorkerPool) Stop() {
+	close(wp.input)
+	wp.wg.Wait()
+	for _, ch := range wp.closers {
+		close(ch)
+	}
+}
+
+func (wp *WorkerPool) AddWork(work string) {
+	wp.input <- work
+}
+
+func (wp *WorkerPool) CloseWorker() error {
+	wp.mutex.Lock()
+	defer wp.mutex.Unlock()
+
+	if len(wp.closers) > 1 {
+		close(wp.closers[len(wp.closers)-1])
+		wp.closers = wp.closers[:len(wp.closers)-1]
+		return nil
+	} else {
+		return errors.New("cannot close worker when in worker pool only one worker")
+	}
+}
+
+func (wp *WorkerPool) AddWorker() {
+	wp.mutex.Lock()
+	defer wp.mutex.Unlock()
+
+	closeChan := make(chan struct{})
+	wp.closers = append(wp.closers, closeChan)
+
+	wp.wg.Add(1)
+	go func(closeChan chan struct{}, workerID int) {
+		wp.startWorkByWorker(workerID)
+		for {
+			select {
+			case <-closeChan:
+				wp.endWorkByWorkerWithClose(workerID)
+				wp.wg.Done()
+				return
+			default:
+				if work, ok := <-wp.input; ok {
+					wp.workByWorker(workerID, work)
+				} else {
+					wp.endWorkByWorker(workerID)
+					wp.wg.Done()
+					return
+				}
+			}
+		}
+	}(closeChan, len(wp.closers))
+}
+
+func (wp *WorkerPool) startWorkByWorker(workerID int) {
+	fmt.Printf("worker № %d start work\n", workerID)
+}
+
+func (wp *WorkerPool) workByWorker(workerID int, input string) {
+	time.Sleep(workTime)
+	fmt.Printf("worker № %d recive %s\n", workerID, input)
+}
+
+func (wp *WorkerPool) endWorkByWorker(workerID int) {
+	fmt.Printf("worker № %d end work\n", workerID)
+}
+
+func (wp *WorkerPool) endWorkByWorkerWithClose(workerID int) {
+	fmt.Printf("worker № %d end work with close\n", workerID)
+}
